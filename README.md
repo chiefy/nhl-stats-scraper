3b92ddeb6aa2726ada30cd813d3447ccc24dcaa3
Christopher Chief Najewicz <chief@beefdisciple.com>
2015-03-08 09:53:26 -0400

    Refactoring some methods. Stuff is finally working properly. Adding README

---
+++ README.md
@@ -0,0 +1,17 @@
+hockey stats scraper
+------------
+This is a proof of concept (PoC) scraper for the new advance stats section on a certain hockey website.
+
+Scraping may or may not be against certain ToS, so before you use this, be sure to consult the specific website's Terms of Service.
+
+I do not condone or encourage site scraping, this is simply a programming exercise / PoC.
+
+## Requirements
+ - node
+ - npm
+
+## To Run
+Clone repo and then 
+```bash
+$ npm run
+```
--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 var Scraper = require('./lib/Scraper');
 
-var skaterScraper = new Scraper({
-    type: 'skater'
+// Regular Season
+(new Scraper({
+	type: 'team'
+})).scrapeAllSeasons(function() {
+	console.info('Team Regular Season Done!');
 });
 
-skaterScraper.scrape();
+// Playoffs
+(new Scraper({
+	type: 'team',
+	game_type: 3
+})).scrapeAllSeasons(function() {
+	console.info('Team Playoffs Done!');
+});
+
+// Regular Season
+(new Scraper({
+	type: 'skater'
+}).scrapeAllSeasonsAllViews(function() {
+	console.info('Skater Regular Season Done!');
+}));
+
+// Playoffs
+(new Scraper({
+	type: 'skater',
+	game_type: 3
+}).scrapeAllSeasonsAllViews(function() {
+	console.info('Skater Playoffs Done!');
+}));
